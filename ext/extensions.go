b1211d062c8a047965eab2464b32afb4d8a3c698
Denis Panfilov <flaticols@gmail.com>
2022-04-20 23:34:47 +0300

    Added extensions

---
+++ ext/extensions.go
@@ -0,0 +1,34 @@
+package ext
+
+import (
+	g "github.com/maragudk/gomponents"
+	"github.com/maragudk/gomponents/html"
+)
+
+func JsonEnc() g.Node {
+	return html.Script(html.Src("https://unpkg.com/htmx.org/dist/ext/json-enc.js"),
+		g.Attr("crossorigin", "anonymous"),
+		g.Attr("defer"),
+	)
+}
+
+func ClassTools() g.Node {
+	return html.Script(html.Src("https://unpkg.com/htmx.org/dist/ext/class-tools.js"),
+		g.Attr("crossorigin", "anonymous"),
+		g.Attr("defer"),
+	)
+}
+
+func ServerSentEvents() g.Node {
+	return html.Script(html.Src("https://unpkg.com/htmx.org/dist/ext/sse.js"),
+		g.Attr("crossorigin", "anonymous"),
+		g.Attr("defer"),
+	)
+}
+
+func AlpineMorph() g.Node {
+	return html.Script(html.Src("https://unpkg.com/htmx.org/dist/ext/alpine-morph.js"),
+		g.Attr("crossorigin", "anonymous"),
+		g.Attr("defer"),
+	)
+}
